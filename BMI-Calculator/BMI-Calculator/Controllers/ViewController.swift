4b8ba59303eba23fe87cbc4c915680e5a3e165b6
Jules <ju.pabst@gmail.com>
2024-01-03 15:39:41 +0100

    meters and kg change when sliders are moved

--- BMI-Calculator/BMI-Calculator/Controllers/ViewController.swift
+++ BMI-Calculator/BMI-Calculator/Controllers/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+ 
+    @IBOutlet var heightLabel: UILabel!
+    
+    @IBOutlet var heightSlider: UISlider!
+    
+    @IBOutlet var weightLabel: UILabel!
+    
+    @IBOutlet var weightSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        heightLabel.text = "1.5 m"
+        
+        weightLabel.text = "50 kg"
     }
 
-
+    @IBAction func adjustHeightSlider(_ sender: UISlider) {
+        heightLabel.text = String(format: "%.2f m", sender.value)
+    }
+    
+    @IBAction func adjustWeightSlider(_ sender: UISlider) {
+        weightLabel.text = String(format: "%.1f kg", sender.value)
+    }
+    
 }
 
